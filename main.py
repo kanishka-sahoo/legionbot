b8bd5515c9956df4d7e267aa2d83e00920dd3c4b
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2024-01-22 13:39:11 +0530

    Added spacing for easy readability

--- main.py
+++ main.py
@@ -25,24 +25,29 @@ async def _ping(ctx):
 async def _joindate(ctx, *, member: discord.Member):
     await ctx.send(f"{member} joined on {member.joined_at}")
 
+
 @bot.command(name="say")
-async def say(ctx,*what):
+async def say(ctx, *what):
     """Repeats what the users enters"""
     if len(what):
         await ctx.send(" ".join(what))
     else:
         await ctx.send("Say What?")
 
+
 @bot.command(name="serverinfo")
 async def serverinfo(ctx):
     """Displays Server information"""
-    server=ctx.guild
-    await ctx.send(f'Server Name: {server.name}\nServer ID: {server.id}\nMembers: {server.member_count}')
+    server = ctx.guild
+    await ctx.send(f'Server Name: {server.name}\n\
+        Server ID: {server.id}\nMembers: {server.member_count}')
+
 
 @bot.command(name="roll")
-async def roll(ctx, sides: int=6):
+async def roll(ctx, sides: int = 6):
     """Rolls a random number by default is till 6"""
-    num=random.randint(1,sides)
+    num = random.randint(1, sides)
     await ctx.send(f'You rolled a {num} on a {sides} sided dice!')
 
 bot.run(config["TOKEN"])
+
